ed568bfae4de2ecbeb0f7b62aaa45956365b0046
Oleksii.Prudkyi <Oleksii.Prudkyi@54f0d219-e743-0410-aa80-518fe994fbb8>
2008-05-29 23:27:43 +0000

    MerriamWebster:  - Thesaurus  - Spanish-English-Spanish dictionary  - added not worked medical (other engine)

--- trunk/Translate.Net/source/TranslateLib/MerriamWebster/MerriamWebsterThesaurus.cs
+++ trunk/Translate.Net/source/TranslateLib/MerriamWebster/MerriamWebsterThesaurus.cs
@@ -37,29 +37,37 @@
 #endregion
 
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Web; 
+using System.Text; 
+using System.Globalization;
+
 
 namespace Translate
 {
 	/// <summary>
-	/// Description of GoogleService.
+	/// Description of MerriamWebsterTranslator.
 	/// </summary>
-	public class MerriamWebsterService : Service
+	
+	[SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
+	public class MerriamWebsterThesaurus : SynonymsDictionary
 	{
-		public MerriamWebsterService()
+		public MerriamWebsterThesaurus()
 		{
-			Url = new Uri("http://www.merriam-webster.com");
-			Name = "merriam_webster";
-			CompanyName = "Merriam-Webster, Inc.";
-			Copyright = "Copyright © 2007 Merriam-Webster, Incorporated";
-			IconUrl = new Uri("http://translateclient.googlepages.com/merriamwebster.png");
-			FullName = "Merriam-Webster dictionary";
+			//CharsLimit = 255;
+			Name = "_thesaurus";
+		
+			AddSupportedTranslation(new LanguagePair(Language.English, Language.English));
+			AddSupportedTranslation(new LanguagePair(Language.English_US, Language.English_US));
 			
-			AddMonolingualDictionary(new MerriamWebsterDictionary());			
-			AddMonolingualDictionary(new MerriamWebsterThesaurus());
-			AddMonolingualDictionary(new MerriamWebsterDictionarySearchEngine());
-			//AddMonolingualDictionary(new MerriamWebsterMedicalDictionary());
-			AddBilingualDictionary(new MerriamWebsterSpEnDictionary());
-			
+			AddSupportedSubject(SubjectConstants.Common);
 		}
-	}
+		
+		
+		protected override void DoTranslate(string phrase, LanguagePair languagesPair, string subject, Result result, NetworkSetting networkSetting)
+		{
+			MerriamWebsterUtils.DoTranslate(this, "http://www.merriam-webster.com/thesaurus", "Thesaurus", phrase, languagesPair, subject, result, networkSetting);
+		}
+		
+	} 
 }
