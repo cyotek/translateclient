c7f50e776d4e3ea3b58456759206ebaeaf843e2a
Oleksii.Prudkyi <Oleksii.Prudkyi@54f0d219-e743-0410-aa80-518fe994fbb8>
2008-06-05 22:36:57 +0000

    0.1.3076.37121

--- trunk/Translate.Net/site/jbuild.cmd
+++ trunk/Translate.Net/site/jbuild.cmd
@@ -6,8 +6,8 @@ call ..\bin\Translate.Net.exe -gen_list
 "c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_uk.java 
 "c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 servicesdata_uk.zip servicesdata_uk.class
 
-"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_ru.java 
-"c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 servicesdata_ru.zip servicesdata_ru.class
+rem "c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_ru.java 
+rem "c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 servicesdata_ru.zip servicesdata_ru.class
 
 del *.class
 del *.java
