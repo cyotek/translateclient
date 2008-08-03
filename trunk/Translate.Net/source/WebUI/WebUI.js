06ef4ae226d3dca15802d9726915e7ebdaca0239
Oleksii.Prudkyi <Oleksii.Prudkyi@54f0d219-e743-0410-aa80-518fe994fbb8>
2008-08-03 00:59:00 +0000

    - instead of cusing ComInterop the "removeAllChilds" and "removeElement" javascript functions created and called over WebBrowser.InvokeScript - this improve speed and make more easy further port to Mono - probably avoided exception with ResultBrowser.Clear reported by Junior

---
+++ trunk/Translate.Net/source/WebUI/WebUI.js
@@ -0,0 +1,32 @@
+﻿
+
+//Remove all childs from the element
+function RemoveAllChilds(parentName)
+{
+	var element = document.getElementById(parentName);
+	if(element)
+	{
+		while (element.firstChild) 
+		{
+	  		element.removeChild(element.firstChild);
+		}
+		return true;
+	}
+	return false;
+}
+
+//remove element from document
+function RemoveElement(name)
+{
+	var element = document.getElementById(name);
+	if(element)
+	{
+		var parent = element.parentNode;
+		if(parent)
+		{
+	  		parent.removeChild(element);
+	  		return true;
+		}
+	}
+	return false;
+}
