f853be382bd03cb281a3d4b7ad58a7dc967f9d7d
Oleksii.Prudkyi <Oleksii.Prudkyi@54f0d219-e743-0410-aa80-518fe994fbb8>
2008-09-27 09:06:52 +0000

    Webserv - added audio.png file for show when service allow to listen pronunciation

--- Translate.Net/source/WebUI/postbuild.cmd
+++ Translate.Net/source/WebUI/postbuild.cmd
@@ -3,5 +3,6 @@ copy /Y /B ..\source\WebUI\*.css WebUI
 copy /Y /B ..\source\WebUI\*.asax WebUI
 copy /Y /B ..\source\WebUI\*.aspx WebUI
 copy /Y /B ..\source\WebUI\*.js WebUI
+copy /Y /B ..\source\WebUI\*.png WebUI
 copy /Y /B Translate.WebUI.* WebUI\bin
 
