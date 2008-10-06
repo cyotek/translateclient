44dac3e6e8aee7fc178047010fbce0901748e4f7
Oleksii.Prudkyi <Oleksii.Prudkyi@54f0d219-e743-0410-aa80-518fe994fbb8>
2008-10-06 00:47:23 +0000

    fixes in calling javac - increased heap size

--- trunk/Translate.Net/site/jbuild.cmd
+++ trunk/Translate.Net/site/jbuild.cmd
@@ -1,12 +1,12 @@
 call ..\bin\Translate.Net.exe -gen_list
 
-"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_en.java 
+"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -J-Xms256m -J-Xmx256m -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_en.java 
 "c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 servicesdata_en.zip servicesdata_en.class
 
-"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_uk.java 
+"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -J-Xms256m -J-Xmx256m -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_uk.java 
 "c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 servicesdata_uk.zip servicesdata_uk.class
 
-"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_ru.java 
+"c:\Program Files\Java\jdk1.6.0_04\bin\javac.exe" -J-Xms256m -J-Xmx256m -g:none -encoding UTF16 -source 1.3 -target 1.1 servicesdata_ru.java 
 "c:\Program Files\7-Zip\7z.exe" a -tzip -mx9 servicesdata_ru.zip servicesdata_ru.class
 
 del *.class
