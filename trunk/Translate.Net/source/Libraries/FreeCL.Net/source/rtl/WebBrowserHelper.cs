ccfcdd0045b9bef55f46e801dc03563d4f8398e1
Oleksii.Prudkyi <Oleksii.Prudkyi@54f0d219-e743-0410-aa80-518fe994fbb8>
2009-09-20 03:04:30 +0000

    port to linux: - avoiding monobugs in InvokeScript - couple of fixes to allow it to work https://bugzilla.novell.com/show_bug.cgi?id=509337 - added batch commands (StartBatch, etc) which used to avoid "refresh" bug in mono - https://bugzilla.novell.com/show_bug.cgi?id=508585 - i.e. reloading of all webcontrols on refresh of main form all invokescripts saved as batch and rerun when needed

--- trunk/Translate.Net/source/Libraries/FreeCL.Net/source/rtl/WebBrowserHelper.cs
+++ trunk/Translate.Net/source/Libraries/FreeCL.Net/source/rtl/WebBrowserHelper.cs
@@ -38,6 +38,7 @@
 
 using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace FreeCL.RTL
 {
@@ -113,9 +114,18 @@ namespace FreeCL.RTL
 		public static bool ObjectToBool(object obj)
 		{
 			bool result = false;
-			if(obj != null && obj is bool)
+			if(obj != null)
 			{
-				result = (bool)obj;
+				if(obj is bool)
+				{
+					result = (bool)obj;
+				}
+				else if(obj is string)
+				{
+					string val = (string)obj;
+					if(val != "")
+						result = val[0] == 't';
+				}
 			}
 			return result; 
 		}
@@ -132,8 +142,148 @@ namespace FreeCL.RTL
 		
 		public static object InvokeScript(WebBrowser wBrowser, string scriptName, Object[] args)
 		{
-			object res = GetDocument(wBrowser).InvokeScript(scriptName, args);
+			//MessageBox.Show(scriptName);
+			object res = null;
+			/*if(wBrowser == null || GetDocument(wBrowser) == null)
+				return null;	
+			*/
+			Wait(wBrowser);
+			if(MonoHelper.IsUnix)
+			{
+				if(args != null && args.Length > 0)
+				{
+					string[] strArgs = new string[args.Length];
+					for (int i = 0; i < args.Length; i++) 
+					{
+						if(args[i] != null)
+						{
+							if(args[i] is String)
+								strArgs[i] = "\"" + args[i].ToString().Replace("\"", "\\\\\\\"").Replace("'", "\\\\\\'") + "\"";
+								//strArgs[i] = "\"" + args[i].ToString().Replace("\"", "&dquote;").Replace("'", "&squote;") + "\"";
+								//strArgs[i] = "\"" + args[i].ToString().Replace("\"", "\\\"").Replace("'", "\\\'") + "\"";
+								//strArgs[i] = "\\\"" + args[i].ToString().Replace("\"", "'") + "\\\"";
+								//strArgs[i] = "\\\"" + args[i].ToString().Replace("'", "\\\\\\\"") + "\\\"";
+							else if(args[i] is Boolean)
+								strArgs[i] = (Boolean)args[i]? "true" : "false"; 
+							else
+								strArgs[i] = args[i].ToString();
+						}	
+						else
+							strArgs[i] = "null";
+					}
+					//string scriptNameTmp = "alert(\"" +scriptName+ ":" + String.Join (",", strArgs).Replace("\"", "'") + "\");" + scriptName + "(" + String.Join (",", strArgs) + ");alert";
+					string scriptNameTmp = "true ? " + scriptName + "(" + String.Join (",", strArgs) + ") : alert";
+					//string scriptNameTmp = "alert(\"&#34;\");alert";
+					//if(scriptName == "AddTranslationCell")
+					//	MessageBox.Show(scriptNameTmp);
+					
+					//Console.WriteLine(scriptNameTmp);
+					//if(scriptName != "SetTableStyle")					
+					res = GetDocument(wBrowser).InvokeScript(scriptNameTmp);
+				}
+				else
+				{
+					//res = GetDocument(wBrowser).InvokeScript("alert(typeof " +scriptName+ ");" + scriptName + "();alert");
+					//res = GetDocument(wBrowser).InvokeScript(scriptName + "();" + "alert(\"" +scriptName+ "\");" + "alert");
+					//Console.WriteLine(scriptName);
+					res = GetDocument(wBrowser).InvokeScript(scriptName);
+				}
+				AddToBatch(wBrowser, scriptName, args);
+			}	
+			else
+			{
+				res = GetDocument(wBrowser).InvokeScript(scriptName, args);
+			}	
+			//Console.WriteLine("res : " + res);
 			return res;
+		}
+		
+		public static void ExecCopy(WebBrowser wBrowser)
+		{
+			if(!MonoHelper.IsUnix)
+				 ExecCopyWin32(wBrowser);
+			///TODO:implement this
+		}
+			
+		public static void ExecCopyWin32(WebBrowser wBrowser)
+		{
+			GetDocument(wBrowser).ExecCommand("Copy", false, null);
+		}
+		
+		public static void ExecSelectAll(WebBrowser wBrowser)
+		{
+			if(!MonoHelper.IsUnix)
+				 ExecSelectAllWin32(wBrowser);
+			///TODO:implement this
+		}
+			
+		public static void ExecSelectAllWin32(WebBrowser wBrowser)
+		{
+			GetDocument(wBrowser).ExecCommand("SelectAll", false, null);
+		}
+		
+
+		static Dictionary<WebBrowser, List< KeyValuePair<string, Object[]> > > batches = new Dictionary<WebBrowser, List< KeyValuePair<string, Object[]> > >();
+		static List<WebBrowser> ignoreAddingToBatch = new List<WebBrowser>();
+		
+		public static void StartBatch(WebBrowser wBrowser)
+		{
+			if(!MonoHelper.IsUnix)
+				return;
+			
+			if(batches.ContainsKey(wBrowser))
+				batches[wBrowser].Clear();
+			else
+				batches[wBrowser] = new List<KeyValuePair<string, Object[]> >();
+		}
+
+		public static void AddToBatch(WebBrowser wBrowser, string scriptName, Object[] args)
+		{
+			if(!MonoHelper.IsUnix)
+				return;
+			
+			lock(ignoreAddingToBatch)
+			{
+				if(ignoreAddingToBatch.Contains(wBrowser))
+					return;
+			}
+			if(batches.ContainsKey(wBrowser))
+			{
+				batches[wBrowser].Add(new KeyValuePair<string, Object[]>(scriptName, args));
+			}	
+		}
+		
+		public static void ResetBatch(WebBrowser wBrowser)
+		{
+			if(!MonoHelper.IsUnix)
+				return;
+			
+			batches.Remove(wBrowser);
+		}
+
+		public static void PlayBatch(WebBrowser wBrowser)
+		{
+			if(!MonoHelper.IsUnix)
+				return;
+			
+			if(batches.ContainsKey(wBrowser))
+			{
+				lock(ignoreAddingToBatch) 
+					ignoreAddingToBatch.Add(wBrowser);
+				try
+				{
+					List< KeyValuePair<string, Object[]> > list = batches[wBrowser];
+					foreach(KeyValuePair<string, Object[]> data in list)
+					{
+						InvokeScript(wBrowser, data.Key, data.Value);
+					}
+				}
+				finally
+				{
+					lock(ignoreAddingToBatch) 
+						ignoreAddingToBatch.Remove(wBrowser);
+				}
+			}			
 		}
 		
 	}
